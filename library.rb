0eff828e53e4660d6d4e579c84d2be6e454fa6c7
MakerSquare Student <students@themakersquare.com>
2013-09-06 14:59:55 -0500

    Fixes initialize and attr_accessors, introduces user check_out and library add_book methods

--- library.rb
+++ library.rb
@@ -1,21 +1,46 @@
+
+
+
 class User
     attr_accessor :name
     attr_accessor :books_checked_out
+	
+	def initialize(name)
+		@name = name
+		@books_checked_out = []
+	end
+
+    def check_out(book)
+    	@books_checked_out << book
+
+
+    end
+
 end
 
 class Book
   	attr_accessor :checked_out_length
 
-  def initialize(author, title, description)
-  	@author = author
-  	@title = title
-  	@description = description
-  end
+  	def initialize(author, title, description)
+  		@author = author
+  		@title = title
+  		@description = description
+  	end
 end
 
 class Library
-  	attr_accessor :overdue_books
+  	attr_accessor :library
   	attr_accessor :checked_out_books
+
+  	def initialize
+  		@library = []
+  		@checked_out_books = []
+  	end
+
+  	def add_book(book)
+  		@library << book
+  	end 
+
 end
 
 =begin
