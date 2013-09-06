6cb45c6b2498ed5973cf0138bf01b7231a3bec6a
MakerSquare Student <students@themakersquare.com>
2013-09-06 16:21:45 -0500

    Features new test code from Gilbert, library check_out method.

--- library.rb
+++ library.rb
@@ -10,48 +10,69 @@ class User
 		@books_checked_out = []
 	end
 
-    def check_out(book)
-    	@books_checked_out << book
-
-
-    end
+    
 
 end
 
 class Book
-  	attr_accessor :checked_out_length
+  	attr_accessor :checked_out_length, :status, :borrower
 
   	def initialize(author, title, description)
   		@author = author
   		@title = title
   		@description = description
+  		# @status = "available"
+  		@borrower = nil
   	end
+  	def title
+  		@title
+  	end
+
 end
 
 class Library
-  	attr_accessor :library
-  	attr_accessor :checked_out_books
 
   	def initialize
   		@library = []
-  		@checked_out_books = []
+  		@checked_out = {}
   	end
+
+  	def check_out(book, user)
+    	
+    	if @library.include?(book) # && book.status == "available"
+    		 @library.delete(book)
+    		 @checked_out[book.title] = user
+    		#book.borrower = user
+    		#book.status = "checked out"
+    	else
+    		"I'm sorry, #{book} is not available"	
+    	end
+    end
 
   	def add_book(book)
   		@library << book
   	end 
 
+  	def book_count
+  		@library.count
+  	end
+
 end
 
-=begin
+me = User.new("Joe")
+book1 = Book.new("Kim", "Shaman", "It's about Shamans")
+puts "Book 1 title: #{book1.title}"
 
-user.add(book)
-book.set_info(author, title, description)
-user.check_out(book)
-user.check_in(book)
-book.status = returns available, overdue, checked out or lost
-library.overdue => shows overdue books
+my_library = Library.new
+my_library.add_book(book1)
+puts "Library book count: #{my_library.book_count}"
 
+my_library.check_out("Shaman", "me")
 
-=end
+checked_out = my_library.checked_out?("Shaman")
+puts "Is 'Shaman' checked out? #{checked_out}"
+
+puts "#{me.name} has #{me.books_checked_out.count} books checked out"
+
+puts "#{me.name}'s first book is #{me.books_checked_out.first.title}"
 
